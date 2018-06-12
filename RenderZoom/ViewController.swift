8c8e734a4e6dae3fa53bb1b6b19bbc7b5cafb219
Filip Korski <filip.korski@montrosesoftware.com>
2018-06-12 16:53:57 +0200

    Initial Commit

---
+++ RenderZoom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RenderZoom
+//
+//  Created by Filip Korski on 12/06/2018.
+//  Copyright © 2018 Filip Korski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
